cddd0c8fa74b01ea1018b44c92588a0c24fe65c9
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 15:32:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,16 @@ def sort_array_asc(nums)
  nums.sort
 end
 
+
 def sort_array_desc (nums)
   nums.sort { |x,y| y <=> x } 
 end
+
+
+def sort_array_char_count(characters)
+  characters.sort
+end
+
+
+def swap_elements()
   

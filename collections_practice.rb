61f6106e9d1c3253fcfeff82aa8e8fb286495a79
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 16:23:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,12 +12,14 @@ def sort_array_char_count(characters)
   characters.sort_by { |x| x.downcase}
 end
 
+def swap_elements(nums)
+  
+  nums.swap! 
+end 
+
+
 def swap_elements_from_to(array, index, destination_index)
   array.swap![0,3] 
 end
 
-def swap_elements(nums)
-  
-  nums.sort 
-end 
   
